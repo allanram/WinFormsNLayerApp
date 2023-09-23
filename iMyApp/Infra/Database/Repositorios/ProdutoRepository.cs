2dd8c5b1aa5fe993604205d4874512afd53cc48c
Allan <allanramosdeaguiar805@gmail.com>
2023-09-23 11:31:41 -0300

    Continuacao

--- iMyApp/Infra/Database/Repositorios/ProdutoRepository.cs
+++ iMyApp/Infra/Database/Repositorios/ProdutoRepository.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Dapper;
+using Database.Conexoes;
+using Microsoft.Data.SqlClient;
+using Negocio.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,34 @@ using System.Threading.Tasks;
 
 namespace Database.Repositorios
 {
-    internal class ProdutoRepository
+    public class ProdutoRepository
     {
+
+        public List<Produto>ObterRelatorioEstoque()
+        {
+            var sql = @"SELECT p.IDProduto AS Codigo, 
+                     p.NomeProduto AS Nome,
+                     p.PrecoUnitario AS Preco,
+                     p.UnidadesEmEstoque AS Estoque,
+                     c.NomeCategoria AS Categoria
+                     FROM Produtos p
+                     INNER JOIN Categorias c
+                     ON P.IDCategoria = C.IDCategoria
+                     WHERE P.UnidadesEmEstoque >0";
+            try
+            {
+                using (var connection = new SqlConnection(SqlServer.StrConexao()))
+                {
+                    var resultado = connection.Query<Produto>(sql);
+                    return resultado.ToList();
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
     }
 }
